77f39a176212f0ad811b378fcadf4cbd1def4c70
Shamim Islam <shamimislam12231@gmail.com>
2024-11-07 14:43:59 +0600

    Extracted to new function

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-int main() {
+void introduce() {
   char name[] = "Shamim";
   int age = 20;
   printf("Hello, I am %s. My age is %d", name, age);
   return 0;
+}
+
+int main() {
+  introduce();
 }
